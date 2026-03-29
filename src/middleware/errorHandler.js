85f912b3f8853772cafb250fa4110d43d8305d85
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-03-29 20:53:01 +0300

    Fix problem with exeption

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  return createHttpError(500, err.message);
+  throw createHttpError(500, err.message);
 };
