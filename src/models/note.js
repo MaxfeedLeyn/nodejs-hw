cba0f0131523b2e1c6869e6dfb8a9c53f25ffac4
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-03-29 18:58:49 +0300

    Add schema of note

---
+++ src/models/note.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
