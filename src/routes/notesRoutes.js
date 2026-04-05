21b907ae5478a6d30422d493b724924b67c97539
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-04-05 20:28:21 +0300

    Change folder name to validations

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -7,7 +7,7 @@ import {
   updateNote,
 } from '../controllers/notesController.js';
 import { celebrate } from 'celebrate';
-import { getAllNotesSchema, noteIdSchema, createNoteSchema, updateNoteSchema } from '../validation/noteValidation.js';
+import { getAllNotesSchema, noteIdSchema, createNoteSchema, updateNoteSchema } from '../validations/noteValidation.js';
 
 const router = Router();
 
